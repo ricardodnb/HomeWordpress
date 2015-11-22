3ae56e68c48f9f5efb54ec374f80337922facc90
1090661 <1090661@isep.ipp.pt>
2015-11-22 21:51:44 +0000

    init

--- wp-content/themes/rgtheme/index.php
+++ wp-content/themes/rgtheme/index.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
-  <h2>OLA</h2>
+<div id="content" class="site-content" role="main">
+<?php /* Start the Loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+   <?php the_content(); ?>
+<?php endwhile; ?>
+</div><!-- #content .site-content -->
 
 <?php get_footer(); ?>
